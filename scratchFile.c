4ffc8675f7cb996cea0fff56237f86e72a86491f
george <georgemarkanton@proton.me>
2022-12-13 21:11:15 +0200

    Rewriting hexToBinary from the ground up

--- scratchFile.c
+++ scratchFile.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
+//#include <stdlib.h>
 #include <string.h>
 
-void hexToBinary(char *hexString) {
+void hexToBinary (char *hexString) {
 
-    size_t length = strlen(hexString);
-
-    for (int i = 0; i < length; i++) {
-
-        int hexInteger = strtol(hexString, NULL, 16);
-
-        for (int j = 3; j >= 0; j--) {
-            printf("%d", (hexInteger >> j) & 1);
+    size_t characterLength= strlen(hexString);
+    //int* bitStorage = (int*) malloc(characterLength * 4);
+    int bitStorage[characterLength * 4];
+    int q = 0;
+    for (int i = 0; i < characterLength; i++) {
+        int coefficient = 8;
+        for (int j = 0; j < 4; j++) {
+            bitStorage[j] = (hexString[i] & coefficient);
+            coefficient >>= 1;
         }
+        q += 4;
     }
+    for (int i = 0; i < characterLength * 4; i++) {
+        printf("%d", bitStorage[i]);
+    }
+    //free(bitStorage);
 }
 
-int main(void) {
-
-    char hexString[16] = {0};
-
-    printf("Enter a hexadecimal number: \n");
-
-    scanf("%s", hexString);
-
-    hexToBinary(hexString);
-
-    return (EXIT_SUCCESS);
+int main(void){
+    char test[4];
+    printf("Input test hex: \n");
+    scanf("%s", test);
+    hexToBinary(test);
 }
+
+
