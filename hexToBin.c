aebf7aacdb83f3b028114073a60fca3cda014115
george <georgemarkanton@proton.me>
2022-12-19 11:33:06 +0200

    Converted to function (no support for non 4-digit numbers yet)

--- hexToBin.c
+++ hexToBin.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (void) {
+/* Function that takes a hex number as an unsigned short input, converts it to binary bits (1 & 0), stores said bits in memory and returns a pointer with the memory address. */
 
-    unsigned int hexNumber, digitBuffer, separatorMask = 0xf000;
+int* hexToBin (const unsigned short *hexInput) {
 
-    printf("Input hexadecimal number: \n");
-    scanf("%x", &hexNumber);
+    unsigned short digitBuffer, separatorMask = 0xf000;
+    unsigned short hexNumber = *hexInput;
 
-    for (int i = 0; i < 4; i++) {
+    unsigned short digitCount = 0;
+    digitBuffer = hexNumber;
+
+    while (digitBuffer) { // determining the number of digits of the inputted hex number
+        digitCount++;
+        digitBuffer >>= 4;
+    }
+
+    unsigned short quadOffset = 0;
+    int *bitStorage = (int*) malloc(sizeof(int) * digitCount);
+
+    for (int i = 0; i < digitCount; i++) {
         digitBuffer = (hexNumber & separatorMask);  // Using bit-masking to zero out all digits besides the desired one
 
-        for (int j = 3 - i; j > 0; j--) {   // Bit-shifting (3 - digits already processed) times to remove excess zeros, perfectly isolating the desired digit
+        for (int j = (digitCount -1) - i; j > 0; j--) {   // Bit-shifting ( (number of digits - 1) - digits already processed) times to remove excess zeros, perfectly isolating the desired digit
             digitBuffer >>= 4;
         }
 
@@ -19,15 +30,32 @@ int main (void) {
 
         for (int k = 0; k < 4; k++) {   // Using another bit-mask to get the value of each individual bit of the isolated digit and printing it in essentially binary form.
             if (digitBuffer & binaryMask) {
-                putchar('1');
+                bitStorage[k + quadOffset] = 1;
             }
             else {
-                putchar('0');
+                bitStorage[k + quadOffset] = 0;
             }
             binaryMask >>= 1;
         }
         separatorMask >>= 4;
+        quadOffset += 4;
     }
 
+    return (bitStorage);
+}
+
+int main (void) {
+
+    unsigned short hexNumber;
+    int* bitStorage = NULL;
+    printf("Input hex number: \n");
+    scanf("%hx", &hexNumber);
+    bitStorage = hexToBin(&hexNumber);
+
+    for (int i = 0; i < 16; i++) {
+        printf("%d", bitStorage[i]);
+    }
+
+    free(bitStorage);
     return (EXIT_SUCCESS);
 }
